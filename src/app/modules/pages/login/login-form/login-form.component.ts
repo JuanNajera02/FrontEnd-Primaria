f4d7b66a3bfa84300201fa4d4e450e0565dd796f
JuanNajera02 <jpnajeracastro@hotmail.com>
2024-03-05 16:48:44 -0700

    se hizo el diseño del log-in

--- src/app/modules/pages/login/login-form/login-form.component.ts
+++ src/app/modules/pages/login/login-form/login-form.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Credential } from '../../../../shared/models/Credential';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, MatInputModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+
+  creds: Credential = {
+    usuario: '',
+    password: ''
+  };
+  error: any;
+
+
+  login() {
+
+  }
+
+
 
 }
--- src/app/modules/pages/login/login.component.ts
+++ src/app/modules/pages/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule,LoginFormComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
