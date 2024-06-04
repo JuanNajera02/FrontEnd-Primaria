5e85b89ad957ad731a598284e2a322b62c4efeb4
Elias Melendez Portillo <melendez.portillo.elias@gmail.com>
2024-06-04 16:12:10 -0600

    xd

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,33 +1,34 @@
-import {Component} from '@angular/core';
-import {NavBarComponent } from '../../../core/header/nav-bar/nav-bar.component';
-import {CommonModule} from "@angular/common";
-import {HttpClient} from "@angular/common/http";
-import {ActivatedRoute} from "@angular/router";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../../../core/header/nav-bar/nav-bar.component';
+import { CommonModule } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import * as XLSX from "xlsx";
-
+import { saveAs } from 'file-saver';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, CommonModule, ReactiveFormsModule,FormsModule],
+  imports: [NavBarComponent, CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-
-export class HomeComponent{
-  //VARIABLES
+export class HomeComponent {
+  // VARIABLES
   mostrarIngresos: boolean = true; // Mostrar sección de ingresos por defecto
   mostrarEgresos: boolean = false;
-  fechaInicio: string ;
-  fechaFin: string ;
-  idEscuela: string ;
+  fechaInicio: string;
+  fechaFin: string;
+  idEscuela: string;
 
-  //INFO
+  // INFO
   movimientos: any[] = [];
   Escuela: any = {};
 
-  //Objetos
+  // Objetos
   http: HttpClient;
 
   showIngresos() {
@@ -40,27 +41,24 @@ export class HomeComponent{
     this.mostrarEgresos = true;
   }
 
-  constructor(private route: ActivatedRoute,http: HttpClient) {
+  constructor(private route: ActivatedRoute, http: HttpClient) {
     this.http = http;
 
     this.fechaInicio = "2021-01-01";
-    //this.fechaFin = dateNow() con formato yyyy-mm-dd
     this.fechaFin = new Date().toISOString().split('T')[0];
     this.showIngresos();
     this.idEscuela = this.route.snapshot.queryParams['idEscuela'];
     this.ObtenerMovimientos(this.fechaInicio, this.fechaFin, this.idEscuela);
     this.ObtenerEscuela();
-
   }
 
   ObtenerMovimientos(fechaInicio:string,fechaFin:string,idEscuela:string){
     const api: string = `https://ingresosegresosback-production.up.railway.app/Movimientos/ingresosEgresosPorClasificacion?fechaInicio=${fechaInicio}&fechaFinal=${fechaFin}&idEscuela=${idEscuela}`;
 
-    this.http.get<string[]>(api).subscribe((data) => {
+    this.http.get<any[]>(api).subscribe((data) => {
       this.movimientos = data;
       console.log(this.movimientos);
     });
-
   }
 
   ObtenerEscuela(){
@@ -74,33 +72,33 @@ export class HomeComponent{
 
   calcularSumatoria(tipo: string): number {
     let sumatoria = 0;
-    for (const key in this.movimientos) {
-      if (this.movimientos.hasOwnProperty(key)) {
-        const value = this.movimientos[key][tipo];
-        sumatoria += value;
+    for (const movimiento of this.movimientos) {
+      if (movimiento.tipo === tipo) {
+        sumatoria += movimiento.value;
       }
     }
     return sumatoria;
   }
 
-  buscar(){
+  buscar() {
     this.idEscuela = this.route.snapshot.queryParams['idEscuela'];
     this.ObtenerEscuela();
     this.ObtenerMovimientos(this.fechaInicio, this.fechaFin, this.idEscuela);
   }
 
-  exportar(){
+  exportar() {
     // Crear un arreglo que combine los datos de movimientos y escuela
     const data = [
-      [`FECHA INICIO ${this.fechaInicio} FECHA FIN ${this.fechaFin}`], // Rango de fechas
+      [`FECHA INICIO: ${this.fechaInicio} FECHA FIN: ${this.fechaFin}`], // Rango de fechas
       [], // Agregar una fila vacía para separar los conjuntos de datos
       ["DATOS ESCUELA"], // Encabezado de la sección de escuela
       [ ...Object.keys(this.Escuela)], // Nombres de las propiedades de la escuela
       [...Object.values(this.Escuela)], // Valores de las propiedades de la escuela
       [], // Agregar una fila vacía para separar los conjuntos de datos
-      ["Clasificacion", "Ingreso", "Egreso"], // Encabezado de la sección de movimientos
-      ...Object.entries(this.movimientos).map(([clasificacion, { ingreso, egreso }]) => [clasificacion, ingreso, egreso]),
+      ["Clasificación", "Ingreso", "Egreso"], // Encabezado de la sección de movimientos
+      ...this.movimientos.map(movimiento => [movimiento.clasificacion, movimiento.ingreso, movimiento.egreso]),
     ];
+    
     // Convertir el arreglo de arreglos a una hoja de cálculo de Excel
     const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
 
@@ -114,7 +112,69 @@ export class HomeComponent{
     XLSX.writeFile(wb, 'reporte_datos.xlsx');
   }
 
+  exportToPDF(): void {
+    const doc = new jsPDF();
 
+    // Título principal
+    doc.setFontSize(14);
+    doc.text("SEGUNDO Informe Financiero de las Asociaciones de Padres de Familia", 10, 10);
+    doc.text("Ciclo escolar 2021 - 2022", 10, 18);
+
+    // Encabezado de la escuela
+    doc.setFontSize(12);
+    doc.text(`Escuela: ${this.Escuela.nombre}`, 10, 30);
+    doc.text(`Clave: ${this.Escuela.clave}`, 10, 40);
+    doc.text(`Zona: ${this.Escuela.zona}`, 10, 50);
+    doc.text(`Sector: ${this.Escuela.sector}`, 10, 60);
+    doc.text(`Domicilio: ${this.Escuela.domicilio}`, 10, 70);
+    doc.text(`Localidad: ${this.Escuela.localidad}`, 10, 80);
+    doc.text(`Teléfono: ${this.Escuela.telefono}`, 10, 90);
+    doc.text(`No. de Padre de Familia: ${this.Escuela.padreFamilia}`, 10, 100);
+    doc.text(`Total de Alumnos: ${this.Escuela.totalAlumnos}`, 10, 110);
+    doc.text(`Cuota de Padres de Familia: ${this.Escuela.cuotaPadres.toLocaleString('es-MX', { style: 'currency', currency: 'MXN' })}`, 10, 120);
+    doc.text(`Total de Grupos: ${this.Escuela.totalGrupos}`, 10, 130);
+
+    // Separador
+    doc.line(10, 135, 200, 135);
+
+    // Ingresos Económicos
+    doc.setFontSize(14);
+    doc.text("A. INGRESOS ECONÓMICOS", 10, 150);
+    doc.setFontSize(12);
+    doc.text(`Datos al Día: ${this.fechaFin}`, 10, 160);
+
+    const ingresos = this.movimientos.filter(mov => mov.tipo === 'ingreso');
+    let startY = 170;
+    ingresos.forEach(ingreso => {
+      doc.text(`${ingreso.clasificacion}: ${ingreso.ingreso.toLocaleString('es-MX', { style: 'currency', currency: 'MXN' })}`, 10, startY);
+      startY += 10;
+    });
+
+    doc.setFontSize(12);
+    doc.text(`TOTAL INGRESOS: ${this.calcularSumatoria('ingreso').toLocaleString('es-MX', { style: 'currency', currency: 'MXN' })}`, 10, startY + 10);
+    startY += 20;
+
+    // Separador
+    doc.line(10, startY, 200, startY);
+    startY += 10;
+
+    // Egresos Registrados
+    doc.setFontSize(14);
+    doc.text("B. EGRESOS REGISTRADOS", 10, startY);
+    doc.setFontSize(12);
+    doc.text(`Datos al Día: ${this.fechaFin}`, 10, startY + 10);
+
+    const egresos = this.movimientos.filter(mov => mov.tipo === 'egreso');
+    let egresosStartY = startY + 20;
+    egresos.forEach(egreso => {
+      doc.text(`${egreso.clasificacion}: ${egreso.egreso.toLocaleString('es-MX', { style: 'currency', currency: 'MXN' })}`, 10, egresosStartY);
+      egresosStartY += 10;
+    });
+
+    doc.setFontSize(12);
+    doc.text(`TOTAL EGRESOS: ${this.calcularSumatoria('egreso').toLocaleString('es-MX', { style: 'currency', currency: 'MXN' })}`, 10, egresosStartY + 10);
+
+    // Descargar el archivo PDF
+    doc.save('reporte.pdf');
+  }
 }
-
-
